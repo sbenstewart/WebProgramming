1a07bd3b172c942b9bdf9654253638c46cbe73a3
sbenstewart <sbenstewart@gmail.com>
2018-12-18 19:28:40 +0530

    Finally done

--- multilevel-input-language-bgcolor-change/js/main.js
+++ multilevel-input-language-bgcolor-change/js/main.js
@@ -7,7 +7,25 @@ function chkhover(){
           document.contact_form.terms.disabled == false;
           valid = false;
   }
+  if ($(document.activeElement).attr("type") == "text" || $(document.activeElement).attr("type") == "textarea")
+  {
+    $('link[href="css/style.css"]').prop('disabled', false);
+  }
     setTimeout(chkhover, 500);
 }
 valid=false;
 $(document).ready(chkhover);
+$('link[href="css/style.css"]').prop('disabled', true);
+
+
+function checkFocus() {
+
+  if ($(document.activeElement).attr("type") == "text" || $(document.activeElement).attr("type") == "textarea") {
+
+  //Something's selected
+
+  return true;
+
+ }
+
+}
